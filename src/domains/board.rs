f2fff2e02290bb1f06201f44025608824367aaee
Sebastian Navarro <sebastian.navarro.saavedra@gmail.com>
2024-03-29 23:40:46 -0700

    Added functionality to game_manager

--- src/domains/board.rs
+++ src/domains/board.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 
 
 #[derive(Debug)]
@@ -10,5 +12,32 @@ impl Board {
     pub fn new(size: usize) -> Self {
         let cells = vec![vec!['-'; size as usize]; size as usize];
         return Self { size, cells }
+    }
+
+    pub fn insert_new_symbol(&mut self, row: usize , col: usize, symbol: char) -> bool {
+        if row >= self.size || col >= self.size {
+            println!("row or col should be less than {}", self.size);
+            return false
+        }
+        if self.cells[row as usize][col as usize] != '-' {
+            println!("row and col must be empty");
+            return false
+        }
+
+        self.cells[row as usize][col as usize] = symbol;
+        return true
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in &self.cells {
+            for cell in row {
+                fmt.write_str(&cell.to_string())?;
+                fmt.write_str(" ")?;
+            }
+            fmt.write_str("\n")?;
+        }
+        Ok(())
     }
 }
--- src/services/game_manager.rs
+++ src/services/game_manager.rs
@@ -12,6 +12,83 @@ impl GameManager {
     pub fn new(players: VecDeque<Player>, board_size:usize) -> Self {
         let board = Board::new(board_size);
         let game = Game::new(players, board);
-        Self { game, moves:0 }
+        return Self { 
+            game, 
+            moves:0,
+         }
+    }
+
+    pub fn play_game(&mut self) {
+        while self.game.status != "COMPLETE" {
+
+
+            // get_player_input
+
+            // insert_symbol_in_cell
+
+            // check if player has won
+
+            // check if game is a draw
+
+            // change turn to the next player
+        }
+    }
+
+    // check if player has won
+    pub fn is_row_complete(&self, row: usize, symbol: char) -> bool {
+
+        for i in 0..self.game.board.size {
+            if self.game.board.cells[row as usize][i as usize] != symbol {
+                return false;
+            }
+        }
+
+        return true
+    }
+
+    pub fn is_col_complete(&self, col:usize, symbol: char) -> bool {
+
+        for i in 0..self.game.board.size {
+            if self.game.board.cells[i as usize][col as usize] != symbol {
+                return false;
+            }
+        }
+
+        return true
+    }
+
+    pub fn is_diagonal_complete(&self, symbol: char) -> bool {
+        let n = self.game.board.size;
+
+        let mut symbols_in_diagonal = 0;
+
+        for i in 0..n {
+            if self.game.board.cells[i as usize][i as usize] != symbol {
+                break;
+            }
+            symbols_in_diagonal += 1;
+        }
+
+        if symbols_in_diagonal == n {
+            return true
+        }
+
+        // reverse diagonal
+        let mut symbols_in_diagonal = 0;
+
+        for i in 0..n {
+            if self.game.board.cells[i as usize][n - i - 1 as usize] != symbol {
+                break;
+            }
+            symbols_in_diagonal += 1;
+        }
+
+        return symbols_in_diagonal == n
+    }
+
+    pub fn is_winner(&self, row:usize, col: usize, symbol: char) -> bool {
+        return self.is_row_complete(row, symbol) || 
+        self.is_col_complete(col, symbol) ||
+        self.is_diagonal_complete(symbol)
     }
 }
